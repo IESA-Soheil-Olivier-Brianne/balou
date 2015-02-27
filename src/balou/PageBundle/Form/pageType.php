b71ce7740507990ce5057a41eadf878edbde4450
OlivLaisne <olivier.laisne1@gmail.com>
2015-02-27 10:29:47 +0100

    creation du bundle page

---
+++ src/balou/PageBundle/Form/pageType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace balou\PageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class pageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('isPublished')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'balou\PageBundle\Entity\page'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'balou_pagebundle_page';
+    }
+}
